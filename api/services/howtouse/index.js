a8e3b3e8bc6e0eebaf70d415e2d3d8bcae3d707f
ВВЕДЕНИЕ В МЕТОДЫ ТЕОРИИ ФУНКЦИЙ ПРОСТРАНСТВЕННОГО КОМПЛЕКСНОГО ПЕРЕМЕННОГО <mathsru@gmail.com>
2019-05-17 14:37:47 -0400

    Fixed sync markdown loading to async

--- api/services/howtouse/index.js
+++ api/services/howtouse/index.js
@@ -1,85 +1,24 @@
 import _ from 'lodash';
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
 import cache from 'memory-cache';
 import uuid from 'uuid';
-
-function getMarkdownFiles() {
-  const files = fs
-    .readdirSync(path.join(__dirname, '..', '..', 'static', 'samples'))
-    .filter(fileName => fileName.endsWith('.md'))
-    .reduce((files, fileName) => {
-      files[fileName.substr(0, fileName.length - 3).toLowerCase()] = path.join(__dirname, '..', '..', 'static', 'samples', fileName);
-      return files;
-    }, {});
-
-  return files;
-}
-
-function getMarkdownFileContent(name) {
-  let result = `# File ${name} not found`;
-  const files = getMarkdownFiles();
-  const fileName = files[name] || path.join(__dirname, '..', '..', 'static', 'readme.md');
-  if (fileName != null) {
-    result = fs.readFileSync(fileName).toString();
-  }
-  return result;
-}
-
-function getSampleFileContent(link) {
-  let fileContent = fs.readFileSync(path.join(__dirname, '..', '..', 'static', 'samples', `${link}`)).toString();
-  // remove BOM mark from file
-  if (fileContent.charCodeAt(0) === 0xfeff) {
-    fileContent = fileContent.substr(1);
-  }
-  // remove SSI command used for package primary library development.
-  fileContent = fileContent.replace(/<!-- # include file="\.\.\/\.\.\/src\.primitives\/src\.primitives\.html"-->/g, match => '');
-  fileContent = fileContent.replace(/(\.\.\/\.\.\/packages)/g, match => '/packages');
-  fileContent = fileContent.replace(/(\.\.\/\.\.\/min)/g, match => '/min');
-  fileContent = fileContent.replace(/(\.\.\/images\/photos)/g, match => '/photos');
-  return fileContent;
-}
-
-function getStaticUrl(url) {
-  return `/api/get-sample?name=${url}`;
-}
+import {loadMarkdown, getSampleFileContent} from './markdown';
 
 export default function customService(app) {
   app.use(express.static(path.join(__dirname, '..', '..', 'static')));
 
-  app.use('/load-markdown', (req, res) => {
-    const groups = {};
-    let index = 0;
-    const fileContent = getMarkdownFileContent(req.query.name);
-    const markdown = fileContent.replace(/(\[[\w ]+?\]\([\s\S]+?\)\s*)+/g, match => {
-      index += 1;
-      const samples = [];
-      match = match.replace(/\[([\w ]+?)\]\(([\s\S]+?)\)/g, (str, caption, url) => {
-        if (url.endsWith('.html') && !url.startsWith('http')) {
-          samples.push({
-            caption,
-            url: getStaticUrl(url),
-            content: getSampleFileContent(url)
-          });
-        }
-        return `[${caption}](/${url})\n`;
-      });
-      if (samples.length > 0) {
-        const groupName = `group${index}`;
-        groups[groupName] = samples;
-        return `[group](${groupName})\n`;
-      }
-      return match;
-    });
-    return res.json({
-      markdown,
-      groups
-    });
+  app.use('/load-markdown', async (req, res, next) => {
+    try {
+      var markdown = await loadMarkdown(req.query.name);
+      return res.json(markdown);
+    } catch (e) {
+      next(e)
+    }
   });
 
-  app.use('/get-sample', (req, res) => {
-    const fileContent = getSampleFileContent(req.query.name);
+  app.use('/get-sample', async (req, res) => {
+    const fileContent = await getSampleFileContent(req.query.name);
     return res.send(fileContent);
   });
 
