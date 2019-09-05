09abee89af79a5246a0d93d95d47d3c72b29d276
BasicPrimitives <info@basicprimitives.com>
2019-09-05 19:42:00 -0400

    Added txt to prod config

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -252,7 +252,7 @@ module.exports = {
       maximumFileSizeToCacheInBytes: 8388608,
 
       // Ensure all our static, local assets are cached.
-      staticFileGlobs: [`${path.dirname(assetsPath)}/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2,zip,pdf}`],
+      staticFileGlobs: [`${path.dirname(assetsPath)}/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2,zip,pdf,txt}`],
       stripPrefix: path.dirname(assetsPath),
 
       directoryIndex: '/',
